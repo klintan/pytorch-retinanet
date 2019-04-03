72c8ffa0b9233e5718a6bcfa6cbc4a072547386f
Andreas Klintberg <andreas.klintberg@meltwater.com>
2019-04-03 00:47:19 -0700

    draft of generalized iou loss

--- retinanet/model/loss.py
+++ retinanet/model/loss.py
@@ -24,12 +24,69 @@ def calc_iou(a, b):
 
 
 class GeneralizedIOU(nn.Module):
+    """
+    Implemented from g-darknet
+    https://github.com/generalized-iou/g-darknet/blob/1421328ef53859a94fe8c434565e4838c3a7b03d/scripts/iou_utils.py
+    """
     def __init__(self):
         pass
 
     def forward(self):
         pass
 
+    def intersection(self, a, b):
+        '''
+            input: 2 boxes (a,b)
+            output: overlapping area, if any
+        '''
+        top = max(a[0], b[0])
+        left = max(a[1], b[1])
+        bottom = min(a[2], b[2])
+        right = min(a[3], b[3])
+        h = max(bottom - top, 0)
+        w = max(right - left, 0)
+        return h * w
+
+    def union(a, b):
+        a_area = (a[2] - a[0]) * (a[3] - a[1])
+        b_area = (b[2] - b[0]) * (b[3] - b[1])
+        return a_area + b_area - intersection(a, b)
+
+    def c(self, a, b):
+        '''
+            input: 2 boxes (a,b)
+            output: smallest enclosing bounding box
+        '''
+        top = min(a[0], b[0])
+        left = min(a[1], b[1])
+        bottom = max(a[2], b[2])
+        right = max(a[3], b[3])
+        h = max(bottom - top, 0)
+        w = max(right - left, 0)
+        return h * w
+
+    def iou(self, a, b):
+        '''
+            input: 2 boxes (a,b)
+            output: Itersection/Union
+        '''
+        U = union(a, b)
+        if U == 0:
+            return 0
+        return intersection(a, b) / U
+
+    def giou(self, a, b):
+        '''
+            input: 2 boxes (a,b)
+            output: Itersection/Union - (c - U)/c
+        '''
+        I = intersection(a, b)
+        U = union(a, b)
+        C = c(a, b)
+        iou_term = (I / U) if U > 0 else 0
+        giou_term = ((C - U) / C) if C > 0 else 0
+        # print("  I: %f, U: %f, C: %f, iou_term: %f, giou_term: %f"%(I,U,C,iou_term,giou_term))
+        return iou_term - giou_term
 
 class FocalLoss(nn.Module):
     # def __init__(self):
